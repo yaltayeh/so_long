8f409bd44b2498793ac6401f10a0d8214b70421c
Yacoub Altayeh (42Amman) <yaltayeh@student.42amman.com>
2024-11-25 21:16:11 +0300

    edit garbeg collecter

--- test/garbeg_collecter.c
+++ test/garbeg_collecter.c
@@ -6,25 +6,26 @@
 /*   By: yaltayeh <yaltayeh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 20:33:53 by yaltayeh          #+#    #+#             */
-/*   Updated: 2024/11/25 20:53:34 by yaltayeh         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:13:55 by yaltayeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <ft_stack.h>
-
-
+#include <fcntl.h>
 
 void *ft_malloc(size_t size);
-//static void	ft_free_all() __attribute__((destructor));
+static void	ft_destroy() __attribute__((destructor));
+int	ft_open(const char *name);
 
-static void	ft_free_all()
+
+static void	ft_destroy()
 {
+	ft_open((void *)-1);
 	ft_malloc(-1);
-	ft_printf("Free all.\n");
 }
 
-void *ft_malloc(size_t size)
+void	*ft_malloc(size_t size)
 {
 	void			*ret;
 	static t_stack	*table;
@@ -43,16 +44,38 @@ void *ft_malloc(size_t size)
 	}
 	ret = malloc(size);
 	if (!ret)
-	{
-		ft_stack_clear(&table);
-		return (NULL);
-	}
+		return (ft_malloc(-1));
 	node = ft_init_node((t_data)ret);
 	if (!node)
-	{
-		ft_stack_clear(&table);
-		return (NULL);
-	}
+		return (ft_malloc(-1));
 	ft_stack_tail_push(table, node);
 	return (ret);
+}
+
+
+int	ft_open(const char *name)
+{
+	int				fd;
+	static t_stack	*table;
+	t_node			*node;
+
+	if (name == (void *)-1)
+	{
+		ft_stack_clear(&table);
+		return (-1);
+	}
+	if (!table)
+	{
+		table = ft_init_stack(FT_INT, NULL, NULL, close);
+		if (!table)
+			return (-1);
+	}
+	fd = open(name, O_WRONLY);
+	if (fd == -1)
+		return (-1);
+	node = ft_init_node((t_data)fd);
+	if (!node)
+		return (-1);
+	ft_stack_tail_push(table, node);
+	return (fd);
 }
--- test/test.c
+++ test/test.c
@@ -6,21 +6,23 @@
 /*   By: yaltayeh <yaltayeh@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 20:42:43 by yaltayeh          #+#    #+#             */
-/*   Updated: 2024/11/25 20:44:50 by yaltayeh         ###   ########.fr       */
+/*   Updated: 2024/11/25 21:15:54 by yaltayeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
 void *ft_malloc(size_t size);
+int	ft_open(const char *name);
 
 int main()
 {
 	char *name;
 
-	name = ft_malloc(16);
+	name = ft_malloc(32);
 	if (!name)
 		return (1);
-	ft_strlcpy(name, "Yacoub", -1);
-	ft_printf("name: %s\n", name);
+	ft_strlcpy(name, "test/test.txt", -1);
+	int fd = ft_open(name);
+	ft_fprintf(fd, "Kurkar");
 }
